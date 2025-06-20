814e8797ff248140ae1498e589a83ce12fe87b3e
Pete Chan <pete@puzzlecats.com>
2025-06-20 12:36:45 -0400

    add additional faq for sudoku

--- faq-articles.js
+++ faq-articles.js
@@ -1,19 +1,83 @@
 window.FAQ_ARTICLES = [
   {
-    "id": "restore-data",
+    "id": "wrong-numbers",
     "game": "sudoku",
     "tab": "popular",
-    "title": "Can I restore data to a current or new device?",
-    "updated": "167d",
-    "content": "Subscriptions and purchased items can be restored by logging into the Apple or Google Play account used for the purchase on your device.\n\nHowever, some in-game data, such as statistics (e.g., number of wins or losses), is not linked to your login or nickname and cannot be restored or transferred.\n\nIf you need assistance with restoring specific items, please reach out to us with specific details about what you're looking to recover (like number of Daily Challenges played or last level achieved), and we'll be happy to help!"
+    "title": "Wrong numbers?",
+    "updated": "1d",
+    "content": "Wrong numbers are denoted in red on the grid.\n\nMistakes are tracked in the game when an incorrect number is entered.\n\nIf you wish to play without automatic mistake check Go to Settings and turn on Manually Checking Mistakes"
+  },
+  {
+    "id": "pencil",
+    "game": "sudoku",
+    "tab": "popular",
+    "title": "Turn off small pencil numbers?",
+    "updated": "1d",
+    "content": "If you wish to play without automatic pencils, In Settings, toggle off Autofill Pencil"
+  },
+  {
+    "id": "larger-numbers",
+    "game": "sudoku",
+    "tab": "popular",
+    "title": "Want larger numbers or a new design?",
+    "updated": "1d",
+    "content": "Tap the painter’s palette icon to change font size and theme\n\nToggle Use Classic UI for the old look"
+  },
+  {
+    "id": "found-a-bug",
+    "game": "sudoku",
+    "tab": "popular",
+    "title": "Found a bug?",
+    "updated": "1d",
+    "content": "Tap the mail icon above to send us the bug report, please do not alter any of the pre-populated data"
+  },
+  {
+    "id": "turn-off-sound",
+    "game": "sudoku",
+    "tab": "popular",
+    "title": "Turn off sound effects?",
+    "updated": "1d",
+    "content": "Go to Settings > toggle off Sound"
+  },
+  {
+    "id": "remove-ads",
+    "game": "sudoku",
+    "tab": "popular",
+    "title": "How do I remove ads?",
+    "updated": "1d",
+    "content": "Go to Settings > Turn off advertisements.\n\nPick monthly or annual Premium"
+  },
+  {
+    "id": "still-see-ads",
+    "game": "sudoku",
+    "tab": "popular",
+    "title": "Still see ads after purchase?",
+    "updated": "1d",
+    "content": "Go to Settings > Turn off advertisements > Restore subscription.\n\n Check your device’s subscription settings"
   },
   {
     "id": "report-ad",
     "game": "sudoku",
-    "tab": "before",
+    "tab": "popuplar",
     "title": "How do I report an advertisement?",
-    "updated": "120d",
+    "updated": "1d",
     "content": "To report an advertisement, please take a screenshot and contact our support team with details about the ad and when it appeared."
+  },
+  {
+    "id": "didnt-purchase",
+    "game": "sudoku",
+    "tab": "popular",
+    "title": "Didn’t get a purchase?",
+    "updated": "1d",
+    "content": "Email us via Settings > Help > Contact Us with your receipt"
+  },
+  {
+    "id": "cancel-subscription",
+    "game": "sudoku",
+    "tab": "popular",
+    "title": "How to Cancel a Subscription",
+    "updated": "1d",
+    "content": "On iPhone or iPad (iOS):\n\nOpen the Settings app\nTap your name at the top\nTap Subscriptions\nChoose the subscription you want to cancel\nTap Cancel Subscription\n\nOn Android (Google Play):\nOpen the Play Store app\nTap your profile picture (top right)\nTap Payments & subscriptions\nTap Subscriptions\nChoose the subscription you want to cancel\nTap Cancel subscription"
   },
   {
     "id": "restore-data",
